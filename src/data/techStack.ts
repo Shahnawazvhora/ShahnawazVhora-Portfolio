415b87ec91efb613119345e4d740f3da3ed9ac8b
Shahnawaz Vhora <shahnawazvhora7333@gmail.com>
2024-11-27 20:54:41 +0530

    updated tech images

--- src/data/techStack.ts
+++ src/data/techStack.ts
@@ -1,24 +1,24 @@
 export const techStack = {
   languages: [
-    { name: 'HTML5', icon: './icons/html5.svg' },
-    { name: 'CSS3', icon: './icons/css3.svg' },
-    { name: 'JavaScript', icon: './icons/javascript.svg' },
-    { name: 'JSX', icon: './icons/jsx.svg' },
-    { name: 'TypeScript', icon: './icons/typescript.svg' },
+    { name: 'HTML5', icon: './icons/HTML5.svg' },
+    { name: 'CSS3', icon: './icons/HTML5.svg' },
+    { name: 'JavaScript', icon: './icons/JavaScript.svg' },
+    { name: 'JSX', icon: './icons/JSX.svg' },
+    { name: 'TypeScript', icon: './icons/TypeScript.svg' },
     { name: 'TSX', icon: './icons/TSX.svg' },
-    { name: 'Sass', icon: './icons/sass.svg' }
+    { name: 'Sass', icon: './icons/Sass.svg' }
   ],
   devTools: [
-    { name: 'ReactJS', icon: './icons/reactjs.svg' },
-    { name: 'Angular', icon: './icons/angular.svg' },
-    { name: 'Redux', icon: './icons/redux.svg' },
-    { name: 'Tailwind', icon: './icons/tailwind.svg' },
-    { name: 'NextJS', icon: './icons/nextjs.svg' },
-    { name: 'Material-UI', icon: './icons/material-ui.svg' },
-    { name: 'Bootstrap', icon: './icons/bootstrap.svg' },
-    { name: 'JQuery', icon: './icons/jquery.svg' },
+    { name: 'ReactJS', icon: './icons/ReactJs.svg' },
+    { name: 'Angular', icon: './icons/Angular.svg' },
+    { name: 'Redux', icon: './icons/Redux.svg' },
+    { name: 'Tailwind', icon: './icons/Tailwind.svg' },
+    { name: 'NextJS', icon: './icons/NextJs.svg' },
+    { name: 'Material-UI', icon: './icons/Material-UI.svg' },
+    { name: 'Bootstrap', icon: './icons/BootStrap.svg' },
+    { name: 'JQuery', icon: './icons/JQuery.svg' },
     { name: 'SQL', icon: './icons/SQL.svg' },
-    { name: 'VSCode', icon: './icons/vscode.svg' },
-    { name: 'Git', icon: './icons/git.svg' },
+    { name: 'VSCode', icon: './icons/VSCode.svg' },
+    { name: 'Git', icon: './icons/Git.svg' },
   ]
 } 
